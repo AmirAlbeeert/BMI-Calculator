86ae765a010f8253a57db28bbf662dfd2e6f2a65
AmirAlbeeert <amiralbeert@gmail.com>
2025-05-06 16:15:40 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Prompt the user for their weight
+const weight = Number(prompt("Please Enter Your Weight (in kg):"));
+
+// Prompt the user for their height
+const height = Number(prompt("Please Enter Your Height (in meters):"));
+
+// Calculate BMI using the formula: weight / height^2
+const BMI = weight / height ** 2;
+
+// Display the BMI result to the user with two decimal places
+alert("Your BMI is: " + BMI.toFixed(2));
