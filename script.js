da3cc5d842e4e017973ed347463e0088cdf4433b
AmirAlbeeert <amiralbeert@gmail.com>
2025-05-06 16:42:37 +0330

    initial commit

--- script.js
+++ script.js
@@ -9,15 +9,4 @@ const BMI = weight / height ** 2;
 
 // Display the BMI result to the user with two Decimal places
 alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
-alert("Your BMI is: " + BMI.toFixed(2));
 
